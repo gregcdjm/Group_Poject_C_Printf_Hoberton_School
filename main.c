74c536dafe9cd831f4296bdbaf114332386d1c49
gregcdjm <gregoirecoudrin@gmail.com>
2022-03-14 23:52:34 +0100

    d

--- main.c
+++ main.c
@@ -11,44 +11,45 @@ int main(void)
 {
     _printf("%");
     printf("\n");
-    printf("%");/* "%" => */
+    printf("%");/* "%" =>  */
     printf("\n\n");
     _printf("%%");
     printf("\n");
-    printf("%%");/* "%%" => */
+    printf("%%");/* "%%" => % */
     printf("\n\n");
     _printf("z%%");
     printf("\n");
-    printf("z%%"); /* "z%%" =>  z%*/
+    printf("z%%"); /* "z%%" =>  z% */
     printf("\n\n");
     _printf("%%z");
     printf("\n");
-    printf("%%z");/* "%%z" =>  %z*/
+    printf("%%z");/* "%%z" =>  %z */
     printf("\n\n");
     _printf("z%");
     printf("\n");
-    printf("z%");/* "z%" =>  z*/
+    printf("z%");/* "z%" =>  z */
     printf("\n\n");
     _printf("%z");
     printf("\n");
-    printf("%z");/* "%z" => */
-    printf("\n\n");
+    printf("%z");/* "%z" =>  */
+    printf("\n");
+    printf("\n");
     _printf("z%%\n");
-    printf("z%%\n");/*z%%\n =>  */
+    printf("z%%\n");/*z%%\n => z% */
     printf("\n");
     _printf("%%z\n");
-    printf("%%z\n");/* %%z\n => */
+    printf("%%z\n");/* %%z\n => %z */
     printf("\n");
     _printf("z%\n");
-    printf("z%\n");/* z%\n => */
+    printf("z%\n");/* z%\n => z%*/
     printf("\n");
     _printf("%z\n");
-    printf("%z\n");/* %z\n => */
+    printf("%z\n");/* %z\n => %*/
     printf("\n");
     _printf("%\n");
-    printf("%\n");/* "%\n" => */
-    printf("%\n");
+    printf("%\n");/* "%\n" => %*/
+    printf("\n");
     _printf("%%\n");
-    printf("%%\n");/* "%%\n" => */
+    printf("%%\n");/* "%%\n" => %*/
     return (0);
 }
